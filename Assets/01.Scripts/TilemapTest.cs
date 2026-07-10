543390d5599f82bc23e4137e2a724da9872da8c8
iiru2570 <iiru2570@gmail.com>
2026-07-10 16:43:04 +0900

    Tilemap 좌표 확인
    마우스 좌 클릭 시 마우스 좌표를 읽어서 해당 셀의 월드 좌표에 플레이어 위치 이동

---
+++ Assets/01.Scripts/TilemapTest.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.InputSystem;
+using Unity.VisualScripting;
+
+public class TilemapTest : MonoBehaviour
+{
+
+    private Camera camera;
+    public Tilemap tilemap;
+    public GameObject player;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //tilemap = GetComponent<Tilemap>();
+        camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Vector3 mousePos = new Vector3(Mouse.current.position.ReadValue().x, Mouse.current.position.ReadValue().y, 0);
+            mousePos = camera.ScreenToWorldPoint(mousePos);
+            Vector3Int cellPos = tilemap.WorldToCell(mousePos);
+            Debug.Log(cellPos);
+            Vector3 worldPos = tilemap.CellToWorld(cellPos);
+            worldPos.x += tilemap.cellSize.x / 2f;
+            worldPos.y += tilemap.cellSize.y / 2f;
+            worldPos.z = player.transform.position.z;
+
+            player.transform.position = worldPos;
+        }
+    }
+}
